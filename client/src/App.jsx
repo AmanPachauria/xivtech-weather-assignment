c7fd9d218ee4393c078a249f947fa4bbd708868f
aman <amanpachauria@gmail.com>
2024-02-01 23:52:32 +0530

    add tailwind css

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,7 +6,7 @@ function App() {
   const [temp, setTemp] = useState({});
 
   const handleClick = async () => {
-    const newText = text.split(', ');
+    const newText = text.split(", ");
     const resp = await axios.post(`http://localhost:8000`, {
       city: newText,
     });
@@ -14,24 +14,29 @@ function App() {
   };
 
   return (
-    <div>
+    <div className="flex flex-col items-center justify-center h-screen">
       <h3>Xivtech</h3>
-      <div>
-        <div >
+      <div className="flex flex-col items-center">
+        <div>
           <input
             type="text"
             value={text}
             onChange={(e) => setText(e.target.value)}
+            className="border border-gray-500 p-2"
           />
         </div>
         <div>
-          <button onClick={handleClick}>Submit</button>
+          <button onClick={handleClick} className="bg-blue-500 text-white p-2 mt-2">
+            Submit
+          </button>
         </div>
       </div>
       {Object.keys(temp).length > 0 ? (
-        <div>
+        <div className="mt-4">
           {Object.entries(temp).map(([city, temperature]) => (
-            <p key={city}>Temp - {city}: {temperature}</p>
+            <p key={city} className="mb-2">
+              Temp - {city}: {temperature}
+            </p>
           ))}
         </div>
       ) : (
