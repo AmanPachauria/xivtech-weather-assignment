a45ff79801437c7ed32942176b4be8d6deefe0ba
aman <amanpachauria@gmail.com>
2024-02-01 23:51:38 +0530

    create frontend and use axios request

--- backend/routers/appRouter.js
+++ backend/routers/appRouter.js
@@ -12,7 +12,6 @@ appRouter.post("/", async (req, res) => {
   let resp = {
     weather: {}
   };
-  console.log(req.body);
   await Promise.all(req.body.city.map(async (e)=>{
       const resp1 = await axios.get(
         `https://api.openweathermap.org/data/2.5/weather?q=${e}&units=metric&appid=${KEY}`
@@ -21,7 +20,6 @@ appRouter.post("/", async (req, res) => {
      resp.weather[e]=(resp1.data.main.temp)+"C";
    })
   );
-  console.log(resp);
   res.send(resp);
 });
 
--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,44 @@
-import React from 'react'
+import { useState } from "react";
+import axios from "axios";
 
-export default function App() {
+function App() {
+  const [text, setText] = useState("");
+  const [temp, setTemp] = useState({});
+
+  const handleClick = async () => {
+    const newText = text.split(', ');
+    const resp = await axios.post(`http://localhost:8000`, {
+      city: newText,
+    });
+    setTemp(resp.data.weather);
+  };
+
   return (
-    <div>App</div>
-  )
+    <div>
+      <h3>Xivtech</h3>
+      <div>
+        <div >
+          <input
+            type="text"
+            value={text}
+            onChange={(e) => setText(e.target.value)}
+          />
+        </div>
+        <div>
+          <button onClick={handleClick}>Submit</button>
+        </div>
+      </div>
+      {Object.keys(temp).length > 0 ? (
+        <div>
+          {Object.entries(temp).map(([city, temperature]) => (
+            <p key={city}>Temp - {city}: {temperature}</p>
+          ))}
+        </div>
+      ) : (
+        <></>
+      )}
+    </div>
+  );
 }
+
+export default App;
